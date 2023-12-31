572e458a6af4c56761574bee6ee27e4a9fd9f965
Ernest Cui <ecuiships@126.com>
2023-12-31 23:21:29 +0800

    init: add ascon and tcp_bio

---
+++ src/modules/bio/socket/tcp_bio.c
@@ -0,0 +1,27 @@
+#include "tcp_bio.h"
+
+bool TcpSend(const int fd, const char *buffer, const size_t send_size)
+{
+    const ssize_t sent_size = send(fd, buffer, send_size, 0);
+    return sent_size == send_size;
+}
+
+bool TcpRecv(const int fd, char *buffer, const size_t recv_size)
+{
+    size_t received_size = 0;
+
+    while (received_size < recv_size)
+    {
+        const size_t remaining_receive_size = recv_size - received_size;
+        const ssize_t current_receive_size = recv(
+            fd, buffer + received_size, remaining_receive_size, 0);
+        if (current_receive_size == -1)
+        {
+            return false;
+        }
+
+        received_size += current_receive_size;
+    }
+
+    return true;
+}
---
+++ src/modules/bio/socket/tcp_bio.h
@@ -0,0 +1,11 @@
+#ifndef TCP_BIO_H_
+#define TCP_BIO_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <sys/socket.h>
+
+bool TcpSend(const int fd, const char *buffer, const size_t send_size);
+bool TcpRecv(const int fd, char *buffer, const size_t recv_size);
+
+#endif
