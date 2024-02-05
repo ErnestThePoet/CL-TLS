07c0551fb93c5e86e9e441986721f9df403ef1a0
Ernest Cui <ecuiships@126.com>
2024-02-06 00:02:02 +0800

    chore: update mqtt header

--- src/modules/mqtt/mqtt_header.h
+++ src/modules/mqtt/mqtt_header.h
@@ -3,25 +3,32 @@
 
 #include <stdint.h>
 
-typedef struct
-{
-    uint8_t flags;
-    uint8_t remaining_length;
-} MqttHeaderCommon;
+/**************************************************************
+ * MQTT Fixed Header
+ *
+ * ---------------------------------------------------------
+ * | bit | 7 | 6 | 5 | 4 |     3     |  2  |  1  |    0    |
+ * ---------------------------------------------------------
+ * | 1B  | Message Type  | DUP Flag  | QOS Level | RETAIN  |
+ * ---------------------------------------------------------
+ * | 1B  | Remaining Length                                |
+ * ---------------------------------------------------------
+ *
+ **************************************************************/
 
-#define MQTT_TYPE_CONNECT 1
-#define MQTT_TYPE_CONNACK 2
-#define MQTT_TYPE_PUBLISH 3
-#define MQTT_TYPE_PUBACK 4
-#define MQTT_TYPE_PUBREC 5
-#define MQTT_TYPE_PUBREL 6
-#define MQTT_TYPE_PUBCOMP 7
-#define MQTT_TYPE_SUBSCRIBE 8
-#define MQTT_TYPE_SUBACK 9
-#define MQTT_TYPE_UNSUBSCRIBE 10
-#define MQTT_TYPE_UNSUBACK 11
-#define MQTT_TYPE_PINGREQ 12
-#define MQTT_TYPE_PINGRESP 13
-#define MQTT_TYPE_DISCONNECT 14
+#define MQTT_MSG_TYPE_CONNECT 1
+#define MQTT_MSG_TYPE_CONNACK 2
+#define MQTT_MSG_TYPE_PUBLISH 3
+#define MQTT_MSG_TYPE_PUBACK 4
+#define MQTT_MSG_TYPE_PUBREC 5
+#define MQTT_MSG_TYPE_PUBREL 6
+#define MQTT_MSG_TYPE_PUBCOMP 7
+#define MQTT_MSG_TYPE_SUBSCRIBE 8
+#define MQTT_MSG_TYPE_SUBACK 9
+#define MQTT_MSG_TYPE_UNSUBSCRIBE 10
+#define MQTT_MSG_TYPE_UNSUBACK 11
+#define MQTT_MSG_TYPE_PINGREQ 12
+#define MQTT_MSG_TYPE_PINGRESP 13
+#define MQTT_MSG_TYPE_DISCONNECT 14
 
 #endif
