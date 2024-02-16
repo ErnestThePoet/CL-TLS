da427141da101aed6423c424c0382bf26d35da7b
Ernest Cui <ecuiships@126.com>
2024-02-16 16:46:50 +0800

    docs: add doc for AdjustAlloc

--- src/modules/common/util.h
+++ src/modules/common/util.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 
+// behaves like realloc but does not copy existing data when 
+// expanding size to ensure best performance.
 void *AdjustAlloc(void *ptr, const size_t old_size, const size_t new_size);
 
 #endif
