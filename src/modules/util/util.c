4cd6171ae1cf63432288f89496a56cbaf1587975
Ernest Cui <ecuiships@126.com>
2024-02-17 16:56:10 +0800

    chore: cleanup util

--- src/modules/util/util.c
+++ src/modules/util/util.c
@@ -1,18 +1,1 @@
 #include "util.h"
-
-void *AdjustAlloc(void *ptr, const size_t old_size, const size_t new_size)
-{
-    if (old_size > new_size)
-    {
-        return realloc(ptr, new_size);
-    }
-    else if (old_size < new_size)
-    {
-        free(ptr);
-        return malloc(new_size);
-    }
-    else
-    {
-        return ptr;
-    }
-}
