d495ba90531091b88fc02c3f906c6ae9c9969a32
Ernest Cui <ecuiships@126.com>
2024-02-24 22:35:46 +0800

    chore: client args complete

--- src/packages/client/client_args.h
+++ src/packages/client/client_args.h
@@ -1,4 +1,16 @@
 #ifndef CLIENT_ARGS_H_
 #define CLIENT_ARGS_H_
 
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <common/def.h>
+
+typedef struct
+{
+    bool register_client;
+    uint16_t listen_port;
+    char config_file_path[MAX_PATH_LENGTH];
+} ClientArgs;
+
 #endif
