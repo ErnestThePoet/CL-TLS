21bff5c4dcf593b305b1c1437a0484e9503d017c
Ernest Cui <ecuiships@126.com>
2024-02-24 22:35:54 +0800

    chore: parse_client_args complete

--- src/packages/client/parse_client_args.c
+++ src/packages/client/parse_client_args.c
@@ -1,1 +1,83 @@
 #include "parse_client_args.h"
+
+void ParseClientArgs(
+    const int argc, char *argv[], ClientArgs *client_args_ret)
+{
+    int register_client = 0;
+    int listen_port = -1;
+    const char *log_level = NULL;
+    const char *config_file_path = NULL;
+
+    const char *usages[] = {
+        "cltls_client [options]",
+        NULL};
+
+    struct argparse_option options[] =
+        {
+            OPT_HELP(),
+            OPT_GROUP("Register server"),
+            OPT_BOOLEAN('r', "register", &register_client, "register the client from KGC", NULL, 0, 0),
+            OPT_GROUP("Mandatory options to run client"),
+            OPT_INTEGER('p', "port", &listen_port, "listen port", NULL, 0, 0),
+            OPT_GROUP("Optional options"),
+            OPT_STRING('l', "log", &log_level, "log level(ERROR|WARN|INFO), defaults to 'WARN'", NULL, 0, 0),
+            OPT_STRING('c', "config", &config_file_path, "server config file path, defaults to 'config.conf'", NULL, 0, 0),
+            OPT_END(),
+        };
+
+    struct argparse arg_parse;
+    argparse_init(&arg_parse, options, usages, 0);
+    argparse_describe(&arg_parse,
+                      "\nThe client implementation of CL-TLS.",
+                      NULL);
+    argparse_parse(&arg_parse, argc, (const char **)argv);
+
+    if (register_client)
+    {
+        client_args_ret->register_client = true;
+        return;
+    }
+    else
+    {
+        client_args_ret->register_client = false;
+    }
+
+    if (listen_port == -1)
+    {
+        PRINT_ERROR_REQUIRED_OPTION_NOT_PROVIDED("'port'('p')");
+    }
+    else if (listen_port < 0 || listen_port > 65535)
+    {
+        PRINT_ERROR_INVALID_OPTION_VALUE("%d", listen_port, "'port'('p')");
+    }
+
+    if (log_level == NULL || !strcmp(log_level, "WARN"))
+    {
+        kLogLevel = LOG_LEVEL_WARN;
+    }
+    else if (!strcmp(log_level, "ERROR"))
+    {
+        kLogLevel = LOG_LEVEL_ERROR;
+    }
+    else if (!strcmp(log_level, "INFO"))
+    {
+        kLogLevel = LOG_LEVEL_INFO;
+    }
+    else
+    {
+        PRINT_ERROR_INVALID_OPTION_VALUE("%s", log_level, "'log'('l')");
+    }
+
+    if (config_file_path == NULL)
+    {
+        strcpy(client_args_ret->config_file_path, "config.conf");
+    }
+    else if (strlen(config_file_path) >= MAX_PATH_LENGTH)
+    {
+        PRINT_ERROR_INVALID_OPTION_VALUE("%s", config_file_path, "'config'('c')");
+    }
+    else
+    {
+        strcpy(client_args_ret->config_file_path, config_file_path);
+    }
+}
--- src/packages/client/parse_client_args.h
+++ src/packages/client/parse_client_args.h
@@ -1,4 +1,39 @@
 #ifndef PARSE_CLIENT_ARGS_H_
 #define PARSE_CLIENT_ARGS_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include <common/def.h>
+
+#include <argparse/argparse.h>
+
+#include <protocol/cltls/cltls_header.h>
+
+#include "client_args.h"
+
+#define PRINT_ERROR_REQUIRED_OPTION_NOT_PROVIDED(NAME)                \
+    do                                                                \
+    {                                                                 \
+        fprintf(stderr,                                               \
+                "error: required option %s is not provided\n", NAME); \
+        argparse_help_cb_no_exit(&arg_parse, options);                \
+        exit(EXIT_FAILURE);                                           \
+    } while (false)
+
+#define PRINT_ERROR_INVALID_OPTION_VALUE(D, VALUE, NAME)        \
+    do                                                          \
+    {                                                           \
+        fprintf(stderr,                                         \
+                "error: invalid value '" D "' for option %s\n", \
+                VALUE, NAME);                                   \
+        argparse_help_cb_no_exit(&arg_parse, options);          \
+        exit(EXIT_FAILURE);                                     \
+    } while (false)
+
+void ParseClientArgs(
+    const int argc, char *argv[], ClientArgs *client_args_ret);
+
 #endif
