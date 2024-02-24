07eede6995a845f7b1bc503b217ad83f482765ab
Ernest Cui <ecuiships@126.com>
2024-02-24 17:41:02 +0800

    feat: SendApplicationData and SendCloseConnection complete

--- src/modules/protocol/cltls/application.c
+++ src/modules/protocol/cltls/application.c
@@ -1,13 +1,75 @@
 #include "application.h"
 
+void SendCloseConnection(const int socket_fd)
+{
+    uint8_t send_data[CLTLS_CLOSE_CONNECTION_HEADER_LENGTH] = {0};
+    CLTLS_SET_COMMON_HEADER(send_data,
+                            CLTLS_MSG_TYPE_CLOSE_CONNECTION,
+                            0);
+    if (!TcpSend(socket_fd,
+                 send_data,
+                 CLTLS_CLOSE_CONNECTION_HEADER_LENGTH))
+    {
+        LogError("[SEND Close Connection] Failed to send Close Connection");
+    }
+}
+
 bool SendApplicationData(const int socket_fd,
                          const HandshakeResult *handshake_result,
+                         const bool is_client,
                          const ByteVec *data)
 {
+    ByteVec send_buffer;
+
+    const char *current_stage = "SEND Application Data";
+
+    ByteVecInitWithCapacity(&send_buffer, INITIAL_SOCKET_BUFFER_CAPACITY);
+
+    ByteVecResize(&send_buffer,
+                  CLTLS_COMMON_HEADER_LENGTH + data->size + MAX_ENC_EXTRA_SIZE);
+
+    size_t encrypted_length = 0;
+
+    if (!handshake_result->aead->Encrypt(
+            data->data, data->size,
+            CLTLS_REMAINING_HEADER(send_buffer.data), &encrypted_length,
+            NULL, 0,
+            is_client
+                ? handshake_result->client_key
+                : handshake_result->server_key,
+            is_client
+                ? handshake_result->client_npub_iv
+                : handshake_result->server_npub_iv,
+            &handshake_result->iv_length))
+    {
+        LogError("[%s] Encryption of Application Data failed",
+                 current_stage);
+        APPLICATION_SEND_SEND_ERROR_STOP_NOTIFY(
+            CLTLS_ERROR_INTERNAL_EXECUTION_ERROR);
+    }
+
+    ByteVecResize(&send_buffer, CLTLS_COMMON_HEADER_LENGTH + encrypted_length);
+
+    CLTLS_SET_COMMON_HEADER(send_buffer.data,
+                            CLTLS_MSG_TYPE_APPLICATION_DATA,
+                            encrypted_length);
+
+    if (!TcpSend(socket_fd,
+                 send_buffer.data,
+                 send_buffer.size))
+    {
+        LogError("[%s] Failed to send Application Data",
+                 current_stage);
+        APPLICATION_SEND_FREE_RETURN_FALSE;
+    }
+
+    ByteVecFree(&send_buffer);
+    return true;
 }
 
 bool ReceiveApplicationData(const int socket_fd,
                             const HandshakeResult *handshake_result,
+                            const bool is_client,
                             ByteVec *data,
                             bool *connection_closed_ret)
 {
@@ -52,8 +114,7 @@ bool ReceiveApplicationData(const int socket_fd,
         LogError("[%s] Failed to receive message remaining part",
                  current_stage);
 
-        ByteVecFree(&receive_buffer);
-        return false;
+        APPLICATION_RECEIVE_FREE_RETURN_FALSE;
     }
 
     switch (CLTLS_MSG_TYPE(receive_buffer.data))
@@ -69,8 +130,7 @@ bool ReceiveApplicationData(const int socket_fd,
                  GetCltlsErrorMessage(
                      CLTLS_REMAINING_HEADER(receive_buffer.data[0])));
 
-        ByteVecFree(&receive_buffer);
-        return false;
+        APPLICATION_RECEIVE_FREE_RETURN_FALSE;
     default:
         break;
     }
@@ -84,8 +144,12 @@ bool ReceiveApplicationData(const int socket_fd,
             receive_remaining_length,
             data->data, &decrypted_length,
             NULL, 0,
-            handshake_result->client_key,
-            handshake_result->client_npub_iv,
+            is_client
+                ? handshake_result->server_key
+                : handshake_result->client_key,
+            is_client
+                ? handshake_result->server_npub_iv
+                : handshake_result->client_npub_iv,
             &handshake_result->iv_length))
     {
         LogError("[%s] Decryption of Application Data failed",
@@ -95,6 +159,8 @@ bool ReceiveApplicationData(const int socket_fd,
     }
 
     ByteVecResize(data, decrypted_length);
+
+    ByteVecFree(&receive_buffer);
 
     *connection_closed_ret = false;
     return true;
--- src/modules/protocol/cltls/application.h
+++ src/modules/protocol/cltls/application.h
@@ -3,6 +3,13 @@
 
 #include "handshake.h"
 
+#define APPLICATION_SEND_FREE_RETURN_FALSE \
+    do                                     \
+    {                                      \
+        ByteVecFree(&send_buffer);         \
+        return false;                      \
+    } while (false)
+
 #define APPLICATION_RECEIVE_FREE_RETURN_FALSE \
     do                                        \
     {                                         \
@@ -24,6 +31,13 @@
                 CLTLS_ERROR_STOP_NOTIFY_HEADER_LENGTH);           \
     } while (false)
 
+#define APPLICATION_SEND_SEND_ERROR_STOP_NOTIFY(ERROR_CODE)    \
+    do                                                         \
+    {                                                          \
+        APPLICATION_COMMON_SEND_ERROR_STOP_NOTIFY(ERROR_CODE); \
+        APPLICATION_SEND_FREE_RETURN_FALSE;                    \
+    } while (false)
+
 #define APPLICATION_RECEIVE_SEND_ERROR_STOP_NOTIFY(ERROR_CODE) \
     do                                                         \
     {                                                          \
@@ -31,12 +45,16 @@
         APPLICATION_RECEIVE_FREE_RETURN_FALSE;                 \
     } while (false)
 
+void SendCloseConnection(const int socket_fd);
+
 bool SendApplicationData(const int socket_fd,
                          const HandshakeResult *handshake_result,
+                         const bool is_client,
                          const ByteVec *data);
 
 bool ReceiveApplicationData(const int socket_fd,
                             const HandshakeResult *handshake_result,
+                            const bool is_client,
                             ByteVec *data,
                             bool *connection_closed_ret);
 
