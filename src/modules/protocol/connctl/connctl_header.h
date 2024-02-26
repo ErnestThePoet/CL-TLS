16ede173e8f4d0e441b848c450c307ba4b65773a
Ernest Cui <ecuiships@126.com>
2024-02-26 11:40:28 +0800

    chore: remove disconnect req&res

--- src/modules/protocol/connctl/connctl_header.h
+++ src/modules/protocol/connctl/connctl_header.h
@@ -7,12 +7,8 @@
 // connect request to the proxy at the very beginning.
 // Similar to HTTP CONNECT request that browsers send
 // to HTTPS proxy servers.
-// When all data have been sent, the client
-// sends a disconnect request.
 #define CONNCTL_MSG_TYPE_CONNECT_REQUEST 0x00
 #define CONNCTL_MSG_TYPE_CONNECT_RESPONSE 0x10
-#define CONNCTL_MSG_TYPE_DISCONNECT_REQUEST 0x20
-#define CONNCTL_MSG_TYPE_DISCONNECT_RESPONSE 0x30
 
 /******************************************************
  * Connect Request
@@ -27,26 +23,6 @@
 
 /******************************************************
  * Connect Response
- *
- * ---------------------------------------
- * | Message Type                        |  1B
- * ---------------------------------------
- * | Status Code                         |  1B
- * ---------------------------------------
- *
- ******************************************************/
-
-/******************************************************
- * Disconnect Request
- *
- * ---------------------------------------
- * | Message Type                        |  1B
- * ---------------------------------------
- *
- ******************************************************/
-
-/******************************************************
- * Disconnect Response
  *
  * ---------------------------------------
  * | Message Type                        |  1B
