cadb5b69e13cd3376d7dc7315c2920169bd4a069
Ernest Cui <ecuiships@126.com>
2024-02-26 21:26:23 +0800

    chore: add MIN and MAX macros

--- src/modules/util/util.h
+++ src/modules/util/util.h
@@ -6,6 +6,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
 bool Hex2Bin(const char *hex, uint8_t *bin, const size_t bin_size);
 void Bin2Hex(const uint8_t *bin, char *hex, const size_t bin_size);
 
