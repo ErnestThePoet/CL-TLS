3fdc76146e31c4613108aa6fa2901730f7671166
Ernest Cui <ecuiships@126.com>
2024-02-29 21:46:45 +0800

    chore: log bs port on conn failure

--- src/packages/server/server_tcp_request_handler.c
+++ src/packages/server/server_tcp_request_handler.c
@@ -102,8 +102,9 @@ static bool KgcServe(const int socket_fd,
                                     current_port,
                                     &belonging_server_socket_fd))
             {
-                LogError("Cannot connect to belonging server %s",
-                         current_identity_hex);
+                LogError("Cannot connect to belonging server %s on port %hu",
+                         current_identity_hex,
+                         current_port);
                 KGC_SERVE_SEND_REGISTER_RESPONSE_FAILURE;
             }
 
