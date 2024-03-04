66b28a362007daab0aa3037bc6292452b3e14eb1
Ernest Cui <ecuiships@126.com>
2024-03-04 19:15:48 +0800

    chore: add MS macro

--- src/modules/util/util.h
+++ src/modules/util/util.h
@@ -5,9 +5,12 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
+#define MS(CLOCK) (((float)(CLOCK)) / CLOCKS_PER_SEC * 1000)
 
 bool Hex2Bin(const char *hex, uint8_t *bin, const size_t bin_size);
 void Bin2Hex(const uint8_t *bin, char *hex, const size_t bin_size);
